7d65ae6688ddf0a82065b0967ce9b75d3264c2eb
Matthew Riley <matthewriley@interapts-macbook-pro-10.local>
2018-09-12 10:10:30 -0500

    Initial Commit

---
+++ Task Manager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task Manager
+//
+//  Created by Matthew Riley on 9/12/18.
+//  Copyright © 2018 Matthew Riley. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
